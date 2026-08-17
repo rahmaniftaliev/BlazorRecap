fec610ac23b6677564c829c41b3c7b0732871dfa
rahmaniftaliev <rahmaniftaliev@gmail.com>
2026-08-17 08:37:37 +0400

    creation of the project and the first model

---
+++ BlazorFrontEndProject/Models/ProductLiseViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorFrontEndProject.Models
+{
+    public class ProductLiseViewModel
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public string UnitsInStock { get; set; }
+    }
+}
